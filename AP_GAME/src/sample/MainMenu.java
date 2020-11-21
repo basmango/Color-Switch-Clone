44dc7248cada50d94aef56147cd3f9d2cf01d143
itida99 <aditiguptaag99@gmail.com>
2020-11-21 13:02:13 +0530

    will mergw branch

---
+++ AP_GAME/src/sample/MainMenu.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.scene.input.MouseEvent;
+
+public class MainMenu {
+    public void resumeMenu(MouseEvent mouseEvent) {
+    }
+
+    public void playGame(MouseEvent mouseEvent) {
+    }
+
+    public void exitGame(MouseEvent mouseEvent) {
+    }
+}
